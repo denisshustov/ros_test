782e9b730c2e3cf4179084ee0fec6e37fa33d395
Denis Shustov <denis.shustov.1984@gmail.com>
2020-01-31 17:42:20 +0200

    Create motor.ino

---
+++ motor.ino
@@ -0,0 +1,99 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+#include <ArduinoHardware.h>
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *myMotorBack = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotorTurn = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotorForward = AFMS.getMotor(4);
+int speed = 255;
+
+
+void messageCb( const geometry_msgs::Twist& msg){
+  //speed_ang = msg.angular.z;
+  //speed_lin = msg.linear.x;
+  if(msg.linear.x == 0)
+  {
+    Stop();
+    return;
+  }
+  else if(msg.linear.x > 0)
+  {
+    RunForward();
+  }
+  else if(msg.linear.x < 0)
+  {
+    RunBack();
+  }
+
+  if(msg.angular.z > 0)
+  {
+    TurnRight();
+  }
+  else if(msg.angular.z < 0)
+  {
+    TurnLeft();
+  }
+};
+
+ros::NodeHandle nh;
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCb );
+
+
+void setup() {
+  Serial.begin(9600);  
+  while (!Serial);
+  Serial.println("Serial OK");
+  AFMS.begin(); 
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotorBack->setSpeed(speed);
+  myMotorTurn->setSpeed(speed);
+  myMotorForward->setSpeed(speed);
+
+  nh.initNode();
+  nh.subscribe(sub);
+  Serial.println("INIT OK");
+}
+void Stop()
+{
+  myMotorForward->run(RELEASE);
+  myMotorBack->run(RELEASE);
+  Serial.println("STOP");
+}
+void RunBack()
+{
+  myMotorBack->run(BACKWARD);
+  myMotorForward->run(BACKWARD);
+  Serial.println("BACKWARD");
+}
+
+void TurnLeft()
+{
+  myMotorTurn->run(FORWARD);
+  Serial.println("LEFT");
+}
+void TurnRight()
+{
+  myMotorTurn->run(BACKWARD);
+  Serial.println("RIGHT");
+}
+
+void RunForward()
+{
+  myMotorForward->run(FORWARD);
+  myMotorBack->run(FORWARD);
+  Serial.println("FORWARD");
+}
+
+void loop() {
+  nh.spinOnce();
+  delay(20);
+  
+  //counter++;  
+  
+}
